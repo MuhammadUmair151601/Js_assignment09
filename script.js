ae7bf93b38f03a06f8ef675fc83497c38aee7b74
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2024-07-06 13:06:06 +0500

    added the code'

---
+++ script.js
@@ -0,0 +1,63 @@
+
+//1 printing a table from 1 to 10:
+// //let table = +prompt("Enter the number:")
+// for(let i=0; i<=10; i++)
+//     {
+//         console.log(table +"*" + i + "=" + (table * i));
+//     }
+
+//2 Array elements sum finding
+// let Array = [];
+// for (let i = 0; i < 5; i++) {
+//     let number = +prompt("Enter numbers" +(i + 1) +":");
+//     Array.push(number);
+// }
+
+// let sum = 0;
+// for (let i = 0; i < Array.length; i++) {
+//     sum += Array[i];
+// }
+
+// console.log("The sum of the array elements are: " + sum);
+// Take five numbers from the user and store them in an array
+
+//3 Largest value in an array
+// let Array = [];
+// for (let i = 0; i < 5; i++) {
+//     let number = +prompt("Enter number" +(i + 1) +":");
+//     Array.push(number);
+// }
+
+//4 let largest = Array[0];
+// for (let i = 1; i < Array.length; i++) {
+//     if (Array[i] > largest)
+//          {
+//         largest = Array[i];
+//     }
+// }
+//console.log("The largest number in the array is:" + largest);
+
+//printing a string in revese order
+
+// let anystring = prompt("Enter your string:");
+
+// let reversedString = "";
+
+// for (let i = anystring.length - 1; i >= 0; i--) {
+//     reversedString += anystring[i];
+// }
+
+// console.log("Your reversed string is: " + reversedString);
+
+//5 Taking any number from the user and finding the factorial of that number:
+
+// Take a number from the user
+// let num = +prompt("Enter a number:");
+
+// let factorial = 1;
+
+// for (let i = 1; i <= num; i++) {
+//     factorial *= i;
+// }
+
+// console.log("The factorial of " + num + " is: " + factorial);
